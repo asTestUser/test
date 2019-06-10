cec82bfd7c4c7fa25426f3fb1e33003d97fed17c
Emily Chimiak <EChimiak@sid_domain2.com>
2019-06-10 13:21:04 -0400

    a

--- src/test/ControllerTest.java
+++ src/test/ControllerTest.java
@@ -27,7 +27,7 @@ public class ControllerTest{
   	//WebElement searchBox = driver.findElement(By.name("q"));
   	//searchBox.sendKeys("ChromeDriver");
   	//searchBox.submit();
- 	assertEquals("abc", driver.getTitle());
+ 	assertEquals("", driver.getTitle());
  	driver.quit();
    }
 }
